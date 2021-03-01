0a417bfc9ac629b383291406166a8739f410786e
Vando dos reis <vandodosreis2001@gmail.com>
2021-02-28 21:38:31 -0300

    Configurando o firebase

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyAFaRNOB1euVNntNqoixAlSy2Y3fL2pr3k",
+    authDomain: "moveit-javascript.firebaseapp.com",
+    projectId: "moveit-javascript",
+    storageBucket: "moveit-javascript.appspot.com",
+    messagingSenderId: "496661572351",
+    appId: "1:496661572351:web:68984a58950b48166a3aca",
+    measurementId: "G-Z7PJM26TWX"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
